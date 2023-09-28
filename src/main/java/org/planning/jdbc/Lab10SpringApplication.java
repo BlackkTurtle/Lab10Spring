51165db71671715592cc1a04a420cb2ed03eba07
BlackkTurtle <113180669+BlackkTurtle@users.noreply.github.com>
2023-09-29 01:10:16 +0300

    Initial commit

---
+++ src/main/java/org/planning/jdbc/Lab10SpringApplication.java
@@ -0,0 +1,13 @@
+package org.planning.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab10SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab10SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/planning/jdbc/Lab10SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.planning.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab10SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
